481fe4e89f710724023dd2ebcf333aaf65b99283
MUKILTEO\1721000 <NUGGETis1004@gmail.com>
2024-09-18 13:24:49 -0700

    Tiles made and Movement polished Tilemap incoming.

--- Retro Remake/Assets/MovementTime.cs
+++ Retro Remake/Assets/MovementTime.cs
@@ -8,36 +8,53 @@ public class MovementTime : MonoBehaviour
     public KeyCode Right = KeyCode.D;
     public KeyCode Up = KeyCode.W;
     public KeyCode Down = KeyCode.S;
-    public float speed = 3;
-
+    public KeyCode jump = KeyCode.W;
+    public float speed = 3, jumpHeight = 15;
+    
+    private Rigidbody2D _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(Left))
+            // Input.GetKey() is for HOLDING a key
+            // Input.GetKeyDown() is for PRESSING a key
+            // Input.GetKeyUp() is for releasing
+
+        if (Input.GetKey(Left))     // Check for the player HOLDING DOWN the left button
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+            // Get the GameObject's Rigidbody2D component and set its velocity to the left at the given speed
+           _rb.velocity = Vector2.left * speed;
         }
 
-        if (Input.GetKey(Right)) 
+        if (Input.GetKey(Right))    // Check for the player HOLDING DOWN the right button
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+            // Get the GameObject's Rigidbody2D component and set its velocity to the Right at the given speed
+            _rb.velocity = Vector2.right * speed;
         }
        
-        if (Input.GetKey(Up)) 
+        if (Input.GetKey(Up))   // Check for the player HOLDING DOWN the up button
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+            // Get the GameObject's Rigidbody2D component and set its velocity to the up at the given speed
+            _rb.velocity = Vector2.up * speed;
         }
-        if (Input.GetKey(Down)) 
+        if (Input.GetKey(Down))     // Check for the player HOLDING DOWN the down button
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+            // Get the GameObject's Rigidbody2D component and set its velocity to the down at the given speed
+            _rb.velocity = Vector2.down * speed;
         }
 
+        if (Input.GetKey(jump))   // Check for the player PRESSING the jump button
+        {
+            // Get the Gameobject's Rigidbody 2D component and set its velocity to be to the up at given jump height
+            _rb.velocity = Vector2.up * jumpHeight;
+        }
+        
+        
     }
 }
