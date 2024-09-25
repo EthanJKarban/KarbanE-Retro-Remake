656d0fe31240d1bd683d8726935a4dc89c278647
MUKILTEO\1721000 <NUGGETis1004@gmail.com>
2024-09-25 13:26:12 -0700

    Bones + Explosion in progress

--- Retro Remake/Assets/Scripts/BombController.cs
+++ Retro Remake/Assets/Scripts/BombController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
+using System.Security.Claims;
 using UnityEngine;
 
 public class BombController : MonoBehaviour
 {
+    [Header("Bomb")]
     public GameObject bombPrefab;
     public KeyCode inputKey = KeyCode.Space;
     public float bombFuseTime = 3f;
     public int bombAmount = 1;
     private int bombsRemaining;
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 1;
 
     private void OnEnable()
     {
@@ -34,6 +41,20 @@ public class BombController : MonoBehaviour
 
         yield return new WaitForSeconds(bombFuseTime);
 
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionRadius);
+        Destroy(explosion.gameObject, explosionDuration);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
         Destroy(bomb);
         bombsRemaining++;
     }
@@ -45,6 +66,30 @@ public class BombController : MonoBehaviour
             other.isTrigger = false;
         }
     }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
+
+        position += direction;
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, direction, length - 1);
+
+
+
+    }
+
+
+
+
 }
     
         
